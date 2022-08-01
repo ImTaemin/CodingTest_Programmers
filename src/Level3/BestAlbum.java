ec894fb004f02314db81f40c68b4b7086257460b
ImTaemin <kw3135@gmail.com>
2022-08-02 02:39:57 +0900

    코딩테스트 연습_Level3_BestAlbum 완료

--- src/Level3/BestAlbum.java
+++ src/Level3/BestAlbum.java
@@ -1,7 +1,6 @@
 package Level3;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/42579
 public class BestAlbum
@@ -14,15 +13,56 @@ public class BestAlbum
 
     public static int[] solution(String[] genres, int[] plays)
     {
-        int[] answer = {};
-
         // 순위
         // 1. 많이 재생된 장르
         // 2. 장르 내에서 많이 재생된 노래
         // 3. 장르 내에서 재생 횟수가 같으면 고유 번호가 낮은 노래 먼저
 
+        ArrayList<Integer> answer = new ArrayList<>();
 
+        // <장르, 총 재생횟수>
+        Map<String, Integer> numMap = new HashMap<>();
+        // <장르, <고유번호, 재생횟수>>
+        Map<String, HashMap<Integer, Integer>> playCntMap = new HashMap<>();
 
-        return answer;
+        for (int i = 0; i < genres.length; i++)
+        {
+            if(numMap.containsKey(genres[i]))
+            {
+                playCntMap.get(genres[i]).put(i, plays[i]);
+                numMap.put(genres[i], numMap.get(genres[i]) + plays[i]);
+            }
+            else
+            {
+                // 장르별 재생 횟수 저장
+                numMap.put(genres[i], plays[i]);
+
+                // 장르에 속한 <고유번호, 재생횟수> 저장
+                HashMap<Integer, Integer> tmp = new HashMap<>();
+                tmp.put(i, plays[i]);
+                playCntMap.put(genres[i], tmp);
+            }
+        }
+
+        //장르 내에서 많이 재생된 노래를 먼저 수록 -> 내림차순 정렬.
+        ArrayList<String> genreList = new ArrayList<>(numMap.keySet());
+        Collections.sort(genreList, (v1, v2) -> numMap.get(v2) - numMap.get(v1));
+
+        for (String genre : genreList)
+        {
+            HashMap<Integer, Integer> uidCntMap = playCntMap.get(genre);
+            List<Integer> uidCntList = new ArrayList<>(uidCntMap.keySet());
+
+            Collections.sort(uidCntList, (v1, v2) -> uidCntMap.get(v2) - uidCntMap.get(v1));
+
+            answer.add(uidCntList.get(0));
+            if (uidCntList.size() > 1)
+            {
+                answer.add(uidCntList.get(1));
+            }
+        }
+
+        System.out.println(answer);
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
