accee0afb2f83a2bfb27a049e8f2ddc9d021f32e
ImTaemin <kw3135@gmail.com>
2022-08-01 23:53:53 +0900

    코딩테스트 연습_Level3_진행중

---
+++ src/Level3/BestAlbum.java
@@ -0,0 +1,28 @@
+package Level3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42579
+public class BestAlbum
+{
+    public static void main(String[] args)
+    {
+        solution(new String[]{"classic", "pop", "classic", "classic", "pop" }, new int[]{500, 600, 150, 800, 2500});
+    }
+
+
+    public static int[] solution(String[] genres, int[] plays)
+    {
+        int[] answer = {};
+
+        // 순위
+        // 1. 많이 재생된 장르
+        // 2. 장르 내에서 많이 재생된 노래
+        // 3. 장르 내에서 재생 횟수가 같으면 고유 번호가 낮은 노래 먼저
+
+
+
+        return answer;
+    }
+}
